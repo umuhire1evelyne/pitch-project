cba2643dadeba8207a993a059c2c63158d8288c0
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-09-27 15:49:51 +0200

    errors was updated

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,3 +1,11 @@
-from flask import Blueprint
-main = Blueprint('main',__name__)
-from . import views,errors
+from flask import render_template
+from . import main
+
+# Error handler decorator
+@main.app_errorhandler(404)
+def four_Ow_four(error):
+    '''
+    Function to render the 404 error page
+    '''
+    title = '404 page'
+    return render_template('fourOwfour.html', title=title),404
